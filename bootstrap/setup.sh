2159527cac50c6114c698c2cf277913b750de836
Henry Lynam <henry.lynam@gmail.com>
2017-04-29 08:09:58 +0100

    - Install Ansible and run script as root

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -3,21 +3,74 @@
 # Error if variable is unassigned
 set -u
 
+user='tec'
+
+# Prompt to continue
+# http://stackoverflow.com/questions/3231804/in-bash-how-to-add-are-you-sure-y-n-to-any-command-or-alias
+function prompt_continue() {
+	msg="$1 [y/N]"
+	response=''
+	read -r -p "$msg " response
+	case "$response" in
+		[yY][eE][sS]|[yY]) 
+			;;
+		*)
+			exit 1
+			;;
+	esac
+}
+
 # Turn off automatic updates
 function turn_off_automatic_updates() {
-	automaticUpdates=$(gsettings get org.gnome.software download-updates)
+	echo
+	echo "*** Turning off automatic updates for user $user"
+	echo
+	automaticUpdates=$(su -c 'gsettings get org.gnome.software download-updates' $user)
 	if [[ "$automaticUpdates" =~ false ]]; then
-		echo "Automatic updates: Currently off"
+		echo "Automatic updates are off for user $user"
 		return 0
 	fi
 
-	echo "Automatic updates: Turning on"
+	echo "Turning off automatic updates for user $user"
 
-	gsettings set org.gnome.software download-updates false
-	echo "Automatic updates: Restart required..."
-	exit 0
+	su -c 'gsettings set org.gnome.software download-updates false' $user
+	echo "Restart required..."
+	prompt_continue 'Do you want to continue?'
+	shutdown -r now
+}
+
+# Install EPEL repository
+function install_epel_repo() {
+	echo
+	echo '*** Installing EPEL repo'
+	echo
+	yum install epel-release
+}
+
+# Install Ansible
+function install_ansible() {
+	echo
+	echo '*** Installing ansible'
+	echo
+	yum install ansible
+}
+
+# Check Ansible
+function check_ansible() {
+	echo
+	echo '*** Ansible version:'
+	echo
+	ansible --version
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "There is a problem running ansible ($ret)"
+		exit $ret
+	fi
 }
 
 turn_off_automatic_updates
+install_epel_repo
+install_ansible
+check_ansible
 
 
